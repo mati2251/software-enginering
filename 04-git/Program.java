8eabd64eb409ff64373ccb342c8fa0d7b6f9277b
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-10-26 17:37:29 +0200

    Added Program.java

---
+++ 04-git/Program.java
@@ -0,0 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFileExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
