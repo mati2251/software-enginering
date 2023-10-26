ee11eaee240cfb44f517afa21d7d5da38e3ba5c4
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-10-26 17:55:18 +0200

    Added l variable

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,8 +1,9 @@
 import java.io.File;
 import java.io.IOException;
-
+// comment
 public class CreateFileExample {
     public static void main(String[] args) {
+    	int l = 5;
     	for(int i=0; i<10; i++){
     		System.out.print("Hello");
     		System.out.println(i);    		
